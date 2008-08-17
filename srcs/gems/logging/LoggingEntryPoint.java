581eae51846609f91e81160eb30a062b68fd0a3f
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-08-17 18:25:27 +0000

    Javadoc added.

--- srcs/gems/logging/LoggingEntryPoint.java
+++ srcs/gems/logging/LoggingEntryPoint.java
@@ -3,6 +3,12 @@ package gems.logging;
 import static gems.logging.LoggingFacility.NULL_FACILITY;
 
 /**
+ * A convenient entry point to the logging subsystem. It encapsulates
+ * a logger, but provides a facade for creation of logging records and
+ * logging tags. It provides simplified ways how to create logging
+ * records, especially when logging facility or logging severity is
+ * not required.  
+ *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class LoggingEntryPoint {
@@ -56,10 +62,23 @@ public final class LoggingEntryPoint {
 		return logger;
 	}
 
+	/**
+	 * Logs a given object using an empty facility and a default severity.
+	 *
+	 * @param object a logged object.
+	 */
 	public void log(final Object object) {
 		logger.log(new LoggingRecord(object, new LoggingTag(NULL_FACILITY, defaultSeverity)));
 	}
 
+	/**
+	 * Logs a given object using an empty facility and a given severity.
+	 *
+	 * @param object a logged object.
+	 * @param severity a severity.
+	 *
+	 * @throws IllegalArgumentException if {@code severity} is {@code null}.
+	 */
 	public void log(final Object object, final LoggingSeverity severity) {
 		if (severity == null) {
 			throw new IllegalArgumentException();
@@ -67,6 +86,14 @@ public final class LoggingEntryPoint {
 		logger.log(new LoggingRecord(object, new LoggingTag(NULL_FACILITY, severity)));
 	}
 
+	/**
+	 * Logs a given object using a given facility and a default severity.
+	 *
+	 * @param object a logged object.
+	 * @param facility a logging facility.
+	 *
+	 * @throws IllegalArgumentException if {@code facility} is {@code null}.
+	 */
 	public void log(final Object object, final LoggingFacility facility) {
 		if (facility == null) {
 			throw new IllegalArgumentException();
@@ -74,6 +101,15 @@ public final class LoggingEntryPoint {
 		logger.log(new LoggingRecord(object, new LoggingTag(facility, defaultSeverity)));
 	}
 
+	/**
+	 * Logs a given object with given facility and severity.
+	 *
+	 * @param object a logged object.
+	 * @param facility a logging facility.
+	 * @param severity a logging severity.
+	 *
+	 * @throws IllegalArgumentException if any of {@code facility} or {@code severity} is {@code null}.
+	 */
 	public void log(final Object object, final LoggingFacility facility, final LoggingSeverity severity) {
 		if (facility == null) {
 			throw new IllegalArgumentException();
@@ -84,6 +120,12 @@ public final class LoggingEntryPoint {
 		logger.log(new LoggingRecord(object, new LoggingTag(facility, severity)));
 	}
 
+	/**
+	 * Logs a given object with additional logging tags metadata.
+	 *
+	 * @param object a logged object.
+	 * @param tags logging tags metadata.
+	 */
 	public void log(final Object object, final LoggingTag... tags) {
 		logger.log(new LoggingRecord(object, tags));
 	}
