16364e53e7389535fbfd2161203bc155b8023f2e
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-08-20 17:50:52 +0000

    Two additional test.

--- test/gems/filtering/JUnitFilter.java
+++ test/gems/filtering/JUnitFilter.java
@@ -1,11 +1,14 @@
 package gems.filtering;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 /**
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class JUnitFilter {
+
+	private static final Object OBJ = new Object();
 
 	@Test(expected = IllegalArgumentException.class) public void allowAllForbidsNull() {
 		Filter.ALLOW_ALL.allows(null);
@@ -13,6 +16,14 @@ public final class JUnitFilter {
 
 	@Test(expected = IllegalArgumentException.class) public void denyAllForbidsNull() {
 		Filter.DENY_ALL.allows(null);
+	}
+
+	@Test public void allowAllAllows() {
+		Assert.assertTrue(Filter.ALLOW_ALL.allows(OBJ));
+	}
+
+	@Test public void denyAllDenies() {
+		Assert.assertFalse(Filter.DENY_ALL.allows(OBJ));
 	}
 
 }
