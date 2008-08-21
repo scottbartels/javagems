2b9e20c99b6c7bd606f0a938f335881d54357ddc
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-08-21 07:35:37 +0000

    JavaDoc added for some of unit tests.

--- test/gems/filtering/JUnitFilter.java
+++ test/gems/filtering/JUnitFilter.java
@@ -4,26 +4,43 @@ import org.junit.Test;
 import org.junit.Assert;
 
 /**
+ * Unit tests of {@code Filter} interface and its null-implementations.
+ *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
-public final class JUnitFilter {
+@SuppressWarnings({"unchecked"}) public final class JUnitFilter {
 
-	private static final Object OBJ = new Object();
+    /**
+     * A not-null object.
+     */
+    private static final Object OBJECT = new Object();
 
-	@Test(expected = IllegalArgumentException.class) public void allowAllForbidsNull() {
+    /**
+     * Checks whether the {@code ALLOW_ALL} null-implementaiton forbids a {@code null} argument.
+     */
+    @Test(expected = IllegalArgumentException.class) public void allowAllForbidsNull() {
 		Filter.ALLOW_ALL.allows(null);
 	}
 
+    /**
+     * Checks whether the {@code DENY_ALL} null-implementaiton forbids a {@code null} argument.
+     */
 	@Test(expected = IllegalArgumentException.class) public void denyAllForbidsNull() {
 		Filter.DENY_ALL.allows(null);
 	}
 
-	@Test public void allowAllAllows() {
-		Assert.assertTrue(Filter.ALLOW_ALL.allows(OBJ));
+    /**
+     * Checks whether the {@coe ALLOW_ALL} null-implementation allows non-null object.
+     */
+    @Test public void allowAllAllows() {
+		Assert.assertTrue(Filter.ALLOW_ALL.allows(OBJECT));
 	}
 
+    /**
+     * Checks whether the {@coe DENY_ALL} null-implementation denies non-null object.
+     */
 	@Test public void denyAllDenies() {
-		Assert.assertFalse(Filter.DENY_ALL.allows(OBJ));
+		Assert.assertFalse(Filter.DENY_ALL.allows(OBJECT));
 	}
 
 }
