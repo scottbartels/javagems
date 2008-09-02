1ec3c57cccf3d9dbb2f917f6ef3f8ec736c5035c
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-09-02 18:46:20 +0000

    Basic usage example for a logging library.

--- docs/examples/srcs/logging/LoggingBasicUsage.java
+++ docs/examples/srcs/logging/LoggingBasicUsage.java
@@ -3,9 +3,9 @@ package logging;
 import gems.logging.Logger;
 import gems.logging.LoggingEntryPoint;
 import gems.logging.LoggingFacility;
+import gems.logging.LoggingHandler;
 import gems.logging.LoggingSeverity;
 import gems.logging.LoggingTag;
-import gems.logging.formatters.PlainLoggingRecordFormatter;
 import gems.logging.handlers.PrintStreamLoggingHandler;
 import gems.logging.loggers.SynchronousLogger;
 
@@ -14,21 +14,81 @@ import gems.logging.loggers.SynchronousLogger;
  */
 public final class LoggingBasicUsage {
 
-	public static void main(final String[] args) throws Exception {
+	/**
+	 * A logging entry point.
+	 */
+	private static final LoggingEntryPoint LEP;
+
+	static {
+		/*
+		 * At first, a logger is created. This is the real entry point to the
+		 * logging subsystem, a LoggingEntryPoint is only a facade providing
+		 * an easier access to the Logger interface and logging records creation.
+		 */
 		final Logger logger = new SynchronousLogger();
-		logger.addHandler(new PrintStreamLoggingHandler(System.out, new PlainLoggingRecordFormatter()));
-		final LoggingEntryPoint lep = new LoggingEntryPoint(logger);
+		/*
+		 * At third, at least one handler should be provided to ensure
+		 * the output from the logging subsystem.
+		 */
+		final LoggingHandler handler = new PrintStreamLoggingHandler(System.out);
+		/*
+		 * Putting logger and handler together.
+		 */
+		logger.addHandler(handler);
+		/*
+		 * Finally, the logger is encapsulated into the logging entry point.
+		 */
+		LEP = new LoggingEntryPoint(logger);
+	}
 
-		lep.log("Simple message.");
-		lep.log("Simple message with severity.", LoggingSeverity.WARNING);
-		lep.log("Simple message with facility.", LoggingFacility.getFacility("SECURITY"));
-		lep.log("Facility and severity together.", LoggingFacility.getFacility("PERFORMANCE"), LoggingSeverity.INFO);
-		lep.log("The most complex case.",
+	/**
+	 * An example entry point. This is very basic example of logging usage.
+	 *
+	 * @param args command line arguments, completelly ignored.
+	 */
+	public static void main(final String[] args) {
+		/*
+		 * The easiest way how to log something is to put it as a single
+		 * argument to the log() method. Please note that an arbitrary
+		 * object can be use here, not only a string. Even more, 'null'
+		 * is allowed here.
+		 */
+		LEP.log(null);
+		LEP.log(" Hello, world! ");
+		LEP.log(new Object());
+
+		/*
+		 * A pretty obvious use case is to assign a logging severity to a logged message.
+		 */
+		LEP.log("Something is wrong.", LoggingSeverity.WARNING);
+
+		/*
+		 * If there is a need to distinguish among several kinds of logging messages,
+		 * for instance "messages related to performance" and "messages related to security",
+		 * the logging facility may be assigned to logging records explicitly.
+		 */
+		LEP.log("Bad boys are here.", LoggingFacility.getFacility("SECURITY"));
+		LEP.log("Load is too high.", LoggingFacility.getFacility("PERFORMANCE"));
+
+		/*
+		 * Of course, facility and severity can be assigned to a logging message at one go.
+		 */
+		LEP.log("Performance information.", LoggingFacility.getFacility("PERFORMANCE"), LoggingSeverity.INFO);
+
+		/*
+		 * This is the most complex use case: one logging message has assigned more
+		 * facilities, each of them with a different severity. A facility-severity
+		 * in encapsulated in LoggingTag object.
+		 */
+		LEP.log("Too many connections.",
 				new LoggingTag(LoggingFacility.getFacility("PERFORMANCE"), LoggingSeverity.INFO),
-				new LoggingTag(LoggingFacility.getFacility("SECURITY"), LoggingSeverity.WARNING),
-				new LoggingTag(LoggingFacility.NULL_FACILITY, LoggingSeverity.CRITICAL));
+				new LoggingTag(LoggingFacility.getFacility("SECURITY"), LoggingSeverity.WARNING));
 
-		lep.log(new RuntimeException("Ooops.", new RuntimeException("Cause.")), LoggingSeverity.FATAL);
+		/*
+		 * Finally, a special approach is used for exceptions logging.
+		 * A stack trace is generated, including chainded causes.
+		 */
+		LEP.log(new RuntimeException("Ooops.", new RuntimeException("I'm the cause.")), LoggingSeverity.FATAL);
 	}
 
 }
