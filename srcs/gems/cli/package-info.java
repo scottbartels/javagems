b74e669431688cdbe004ffdd62c359601754b399
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-09-02 17:42:17 +0000

    javadoc polishing

--- srcs/gems/cli/package-info.java
+++ srcs/gems/cli/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A tool for command line options handling. Two main types of objects are involved:
+ * A library for command line options handling. Two main types of objects are involved:
  * "options", which represents options designed by a programmer and expected by a program,
  * and "actuators", which represents recognized user inputs typed on command line.
  */
