4278aad09a413685a3891478c569edd4b387eb89
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-10-07 19:43:56 +0000

    Javadoc.

--- srcs/gems/logging/formatters/TimestampFormatter.java
+++ srcs/gems/logging/formatters/TimestampFormatter.java
@@ -10,32 +10,34 @@ import java.util.Date;
  */
 final class TimestampFormatter {
 
-    /**
-     * A formatting pattern.
-     */
-    private final String pattern;
+	/**
+	 * A formatting pattern.
+	 */
+	private final String pattern;
 
-    /**
-     * Creates a new timestamp formatter with a given pattern for {@code java.text.SimpleDateFormat}.
-     *
-     * @param pattern a pattern.
-     * @throws IllegalArgumentException if {@code pattern} is {@code null}.
-     */
-    TimestampFormatter(final String pattern) {
-        if (pattern == null) {
-            throw new IllegalArgumentException();
-        }
-        this.pattern = pattern;
-    }
+	/**
+	 * Creates a new timestamp formatter with a given pattern for {@code java.text.SimpleDateFormat}.
+	 *
+	 * @param pattern a pattern.
+	 *
+	 * @throws IllegalArgumentException if {@code pattern} is {@code null}.
+	 */
+	TimestampFormatter(final String pattern) {
+		if (pattern == null) {
+			throw new IllegalArgumentException();
+		}
+		this.pattern = pattern;
+	}
 
-    /**
-     * Formats a given timestamp. 
-     * @param timestamp
-     * @return
-     */
-    String format(final long timestamp) {
-        final SimpleDateFormat formatter = new SimpleDateFormat(pattern);
-        return formatter.format(new Date(timestamp));
-    }
+	/**
+	 * Formats a given timestamp.
+	 *
+	 * @param timestamp a timestamp.
+	 *
+	 * @return a formatted timestamp.
+	 */
+	String format(final long timestamp) {
+		return new SimpleDateFormat(pattern).format(new Date(timestamp));
+	}
 
 }
