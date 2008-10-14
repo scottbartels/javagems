1f2b54aae47c574b7cb279c8117d500cc47816ee
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-10-14 06:52:22 +0000

    Abstract implementations of logger splitted according their responsibilities: AbstractLogger deals with handlers, AbstractFilteringLogger deals with filtering capabilities.

--- trunk/srcs/gems/logging/loggers/AbstractFilteringLogger.java
+++ trunk/srcs/gems/logging/loggers/AbstractFilteringLogger.java
@@ -1,28 +1,17 @@
 package gems.logging.loggers;
 
 import gems.filtering.Filter;
-import gems.logging.Logger;
-import gems.logging.LoggingHandler;
 import gems.logging.LoggingRecord;
-
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * A skeleton implementation of <em>logger</em>. It ensures the common functionality
  * of adding and providing underlaying <em>logging handlers</em>. A real handling of
  * <em>logging records</em> is still delegated to subclasses. It also adds an ability
- * to filter logging records. 
+ * to filter logging records.
  *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
-abstract class AbstractFilteringLogger implements Logger {
-
-	/**
-	 * Handlers. Unmodifiable defense copy is hold here, so it is safe to provide it outside.
-	 */
-	private volatile List<LoggingHandler> handlers = Collections.emptyList();
+abstract class AbstractFilteringLogger extends AbstractLogger {
 
 	/**
 	 * A filter of logging records.
@@ -41,30 +30,6 @@ abstract class AbstractFilteringLogger implements Logger {
 			throw new IllegalArgumentException();
 		}
 		this.filter = filter;
-	}
-
-	/**
-	 * {@inheritDoc}
-	 *
-	 * @throws IllegalArgumentException if {@code handler} is {@code null}.
-	 */
-	public final synchronized void addHandler(final LoggingHandler handler) {
-		if (handler == null) {
-			throw new IllegalArgumentException();
-		}
-		final List<LoggingHandler> modifiable = new LinkedList<LoggingHandler>(handlers);
-		modifiable.add(handler);
-		handlers = Collections.unmodifiableList(modifiable);
-	}
-
-	/**
-	 * {@inheritDoc} Returned list is unmodifiable. This method never returns {@code null};
-	 * at least an empty list returned, even if no handlers were added to the logger.
-	 *
-	 * @return handlers of the logger.
-	 */
-	public final List<LoggingHandler> getHandlers() {
-		return handlers;
 	}
 
 	/**
