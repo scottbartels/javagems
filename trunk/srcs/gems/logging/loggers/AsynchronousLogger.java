88d4dc5d83fff535c3a5274432bd8df8ba172436
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-10-14 06:48:33 +0000

    Renaming ones more, typo fixed.

--- trunk/srcs/gems/logging/loggers/AsynchronousLogger.java
+++ trunk/srcs/gems/logging/loggers/AsynchronousLogger.java
@@ -16,7 +16,7 @@ import java.util.concurrent.Executors;
  *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
-public final class AsynchronousLogger extends AbstracFilteringtLogger {
+public final class AsynchronousLogger extends AbstractFilteringLogger {
 
 	/**
 	 * An internal asynchronous processor of logging records.
