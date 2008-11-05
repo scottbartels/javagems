eb045c00431faa1064787e205c9967d8ccac68b2
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-11-05 20:47:45 +0000

    Missing JavaDoc added for some unit tests.

--- test/gems/filtering/JUnitFilterChain.java
+++ test/gems/filtering/JUnitFilterChain.java
@@ -4,28 +4,48 @@ import org.junit.Assert;
 import org.junit.Test;
 
 /**
+ * Unit tests for the {@code FilterChain} class.
+ *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class JUnitFilterChain {
 
+	/**
+	 * A filtered object.
+	 */
 	private static final Object OBJ = new Object();
 
+	/**
+	 * hecks whether a {@code null} chaining policy is forbidden.
+	 */
 	@Test(expected = IllegalArgumentException.class) public void constructorForbidsNull() {
 		new FilterChain<Object>(null);
 	}
 
+	/**
+	 * Checks whether an empty chain with satisfy all policy returns {@code false}.
+	 */
 	@Test public void emptyStatisfyAllReturnsFalse() {
 		Assert.assertFalse(new FilterChain<Object>(FilterChainingPolicy.SATISFY_ALL).allows(OBJ));
 	}
 
+	/**
+	 * Checks whether an empty chain with satisfy any policy returns {@code false}.
+	 */
 	@Test public void emptyStatisfyAnyReturnsFalse() {
 		Assert.assertFalse(new FilterChain<Object>(FilterChainingPolicy.SATISFY_ANY).allows(OBJ));
 	}
 
-	@Test(expected = IllegalArgumentException.class) public void addingNullFilterisForbidden() {
+	/**
+	 * Checks whether adding a {@code null} filter to the chain is forbidden.
+	 */
+	@Test(expected = IllegalArgumentException.class) public void addingNullFilterIsForbidden() {
 		new FilterChain<Object>(FilterChainingPolicy.SATISFY_ALL).add(null);
 	}
 
+	/**
+	 * Checks whether the chain with satisfy any policy contiaing only denying filters itself denies.
+	 */
 	@Test public void satisfyAnyRefuses() {
 		final FilterChain<Object> chain = new FilterChain<Object>(FilterChainingPolicy.SATISFY_ANY);
 		chain.add(Filter.DENY_ALL);
@@ -34,6 +54,9 @@ public final class JUnitFilterChain {
 		Assert.assertFalse(filter.allows(OBJ));
 	}
 
+	/**
+	 * Checks whether the chain with satisfy all policy containing only allowing filters itself allows.
+	 */
 	@Test public void satisfyAllAllows() {
 		final FilterChain<Object> chain = new FilterChain<Object>(FilterChainingPolicy.SATISFY_ALL);
 		chain.add(Filter.ALLOW_ALL);
@@ -42,6 +65,9 @@ public final class JUnitFilterChain {
 		Assert.assertTrue(filter.allows(OBJ));
 	}
 
+	/**
+	 * Checks whether the chain with satisfy any policy containing a mixture of filters itself allows.
+	 */
 	@Test public void satisfyAny() {
 		final FilterChain<Object> chain = new FilterChain<Object>(FilterChainingPolicy.SATISFY_ANY);
 		chain.add(Filter.DENY_ALL);
@@ -50,6 +76,9 @@ public final class JUnitFilterChain {
 		Assert.assertTrue(filter.allows(OBJ));
 	}
 
+	/**
+	 * Checks wheter the chain with satisfy all policy containing a mixture of filters itself denies.
+	 */
 	@Test public void satisfyAll() {
 		final FilterChain<Object> chain = new FilterChain<Object>(FilterChainingPolicy.SATISFY_ALL);
 		chain.add(Filter.DENY_ALL);
