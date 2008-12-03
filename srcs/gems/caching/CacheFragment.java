7374c65931446172e1de1d307fe689ca68c98bc2
jozef.babjak <jozef.babjak@gmail.com>
2008-12-03 17:13:27 +0000

    Interfaces clarified. Honestly, I have to use paper and pencil for a design.

--- srcs/gems/caching/CacheFragment.java
+++ srcs/gems/caching/CacheFragment.java
@@ -5,16 +5,16 @@ import gems.Identifiable;
 /**
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
-final class CacheFragment<O extends Identifiable<K>, K> {
+final class CacheFragment<O extends Identifiable<K>, K> implements Cache<O, K> {
 
-	void put(final O object) {
+	public void offer(final O object) {
 		if (object == null) {
 			throw new IllegalArgumentException();
 		}
 		// todo: implement this.
 	}
 
-	O get(final K key) {
+	public O get(final K key) {
 		return null; // todo: implement this. 
 	}
 
