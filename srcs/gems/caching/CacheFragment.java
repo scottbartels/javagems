9e276552e6326e0e16ea6c2b1a1fa8f8c05e8b03
jozef.babjak <jozef.babjak@gmail.com>
2008-12-03 19:10:43 +0000

    CacheLimits are now an immutable object instead of javabean. All current Cache interface implementations use

--- srcs/gems/caching/CacheFragment.java
+++ srcs/gems/caching/CacheFragment.java
@@ -5,9 +5,13 @@ import gems.Identifiable;
 /**
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
-final class CacheFragment<O extends Identifiable<K>, K> implements Cache<O, K> {
+final class CacheFragment<O extends Identifiable<K>, K> extends AbstractLimitedCache<O, K> {
 
 	private final CacheStorage<O, K> storage = new MemoryCacheStorage<O,K>();
+
+	CacheFragment(final CacheLimits limits) {
+		super(limits);
+	}
 
 	public void offer(final O object) {
 		if (object == null) {
