bc6c69d36f093f47a86fbb16646a9ad887d37b8d
jozef.babjak <jozef.babjak@gmail.com>
2008-12-03 17:28:48 +0000

    CacheFragmenter now uses real cache storage.

--- srcs/gems/caching/CacheFragment.java
+++ srcs/gems/caching/CacheFragment.java
@@ -7,15 +7,20 @@ import gems.Identifiable;
  */
 final class CacheFragment<O extends Identifiable<K>, K> implements Cache<O, K> {
 
+	private final CacheStorage<O, K> storage = new MemoryCacheStorage<O,K>();
+
 	public void offer(final O object) {
 		if (object == null) {
 			throw new IllegalArgumentException();
 		}
-		// todo: implement this.
+		storage.store(object);
 	}
 
 	public O get(final K key) {
-		return null; // todo: implement this. 
+		if (key == null) {
+			throw new IllegalArgumentException();
+		}
+		return storage.load(key);
 	}
 
 }
