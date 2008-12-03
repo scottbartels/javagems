bcfd6f588324c3c80a63614d64f814b8b8afd0eb
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2008-12-03 18:30:06 +0000

    CacheLimits documented.

--- trunk/srcs/gems/caching/CacheLimits.java
+++ trunk/srcs/gems/caching/CacheLimits.java
@@ -1,27 +1,57 @@
 package gems.caching;
 
+/**
+ * Cache limits, ie maximal number of cached items and maximal size of cached items. Limits are always non-negative.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
 public final class CacheLimits {
 
+	/**
+	 * Maximal number of cached items.
+	 */
 	private int count = Integer.MAX_VALUE;
 
+	/**
+	 * Maximal size of cached items.
+	 */
 	private long size = Long.MAX_VALUE;
 
+	/**
+	 * Creates a new cache limits holder.
+	 */
 	public CacheLimits() {
 		// really nothing here
 	}
 
-	CacheLimits(final CacheLimits limits, final int fragment) {
+	/**
+	 * Copying constructor. Creates a new cache limits holder, getting
+	 * limits from a given {@code limits} object, but dividing them by
+	 * a given fragmentation.
+	 *
+	 * @param limits copied cache limits.
+	 * @param fragmentation a required fragmentation.
+	 *
+	 * @throws IllegalArgumentException if {@code limits} is {@code null} of if {@code fragmentation} is less than 1.
+	 */
+	CacheLimits(final CacheLimits limits, final int fragmentation) {
 		if (limits == null) {
 			throw new IllegalArgumentException();
 		}
-		if (fragment < 1) {
+		if (fragmentation < 1) {
 			throw new IllegalArgumentException();
 		}
-		this.count = limits.count / fragment;
-		this.size = limits.size / fragment;
+		this.count = limits.count / fragmentation;
+		this.size = limits.size / fragmentation;
 	}
 
-
+	/**
+	 * Sets maximal number of cached items.
+	 *
+	 * @param count new maximal limit of cached items.
+	 *
+	 * @throws IllegalArgumentException if {@code count} is negative.
+	 */
 	public void setCount(final int count) {
 		if (count < 0) {
 			throw new IllegalArgumentException();
@@ -29,10 +59,23 @@ public final class CacheLimits {
 		this.count = count;
 	}
 
+	/**
+	 * Returns maximal number of cached items. This method always returns a non-negative
+	 * number. If the limit was not set, {@code Integer.MAX_VALUE} is returned.
+	 *
+	 * @return maximal number of cached items.
+	 */
 	public int getCount() {
 		return count;
 	}
 
+	/**
+	 * Sets maximal size of cached items.
+	 *
+	 * @param size new maximal size of cached items.
+	 *
+	 * @throws IllegalArgumentException if {@code size} is nagative.
+	 */
 	public void setSize(final long size) {
 		if (size < 0) {
 			throw new IllegalArgumentException();
@@ -40,6 +83,12 @@ public final class CacheLimits {
 		this.size = size;
 	}
 
+	/**
+	 * Returns maximal size of cached items. This method always returns a non-negative
+	 * number. If the limit was not set, {@code Long.MAX_VALUE} is returned.
+	 *
+	 * @return maximal size of cached items.
+	 */
 	public long getSize() {
 		return size;
 	}
