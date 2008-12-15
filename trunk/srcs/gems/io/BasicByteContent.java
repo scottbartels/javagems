f5d20cc09843703e6edf23b2eedeefb9f5fe8f21
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2008-12-15 12:47:59 +0000

    Small improvements, javadoc added.

--- trunk/srcs/gems/io/BasicByteContent.java
+++ trunk/srcs/gems/io/BasicByteContent.java
@@ -1,6 +1,9 @@
 package gems.io;
 
 /**
+ * The 'leaf' implementation of {@code ByteContent}
+ * interface. It holds raw byte content data.  
+ *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class BasicByteContent extends AbstractNonAggregatedByteContent {
@@ -11,7 +14,7 @@ public final class BasicByteContent extends AbstractNonAggregatedByteContent {
 	private final byte[] content;
 
 	/**
-	 * Creates a new instance storing a whole given content.
+	 * Creates a new instance storing the whole given content.
 	 *
 	 * @param content a content.
 	 * @throws IllegalArgumentException if {@code content} is {@code null}.
@@ -23,12 +26,14 @@ public final class BasicByteContent extends AbstractNonAggregatedByteContent {
 	/**
 	 * Creates a new instance storing a given content. Argument {@code length}
 	 * specifies how many bytes from content are valid. Only valid bytes will
-	 * be stored in the created instance.
+	 * be stored in the created instance. A defense copy of the given array is
+	 * created internally, so any subsequent changes of the given array wont
+	 * affect created object.
 	 *
 	 * @param content a content.
 	 * @param length a valid lenght.
 	 * @throws IllegalArgumentException if {@code content} is {@code null}.
-	 * @throws IndexOutOfBoundsException if {@code length} is less than zero or greater than a {@code content} length.
+	 * @throws IndexOutOfBoundsException if {@code length} is negative or greater than a {@code content} length.
 	 */
 	public BasicByteContent(final byte[] content, final int length) {
 		if (content == null) {
@@ -44,10 +49,13 @@ public final class BasicByteContent extends AbstractNonAggregatedByteContent {
 
 	/**
 	 * {@inheritDoc}
+	 *
+	 * @throws IndexOutOfBoundsException if {@code index} is negative or
+	 * if {@code index} is greater than or equal to subcontent length.
 	 */
 	public synchronized byte getByteAt(final int index) {
 		if (index < 0 || index >= length()) {
-			throw new IndexOutOfBoundsException("Wrong index: " + index);
+			throw new IndexOutOfBoundsException(String.valueOf(index));
 		}
 		return content[index];
 	}
