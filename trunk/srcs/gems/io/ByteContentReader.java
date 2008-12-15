5ffec2d275eaf85174a01c7941a23033fb9cd89d
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2008-12-15 15:38:51 +0000

    Working on the reading of byte content from an input stream.

--- trunk/srcs/gems/io/ByteContentReader.java
+++ trunk/srcs/gems/io/ByteContentReader.java
@@ -1,8 +1,5 @@
 package gems.io;
 
-import java.io.IOException;
-import java.io.InputStream;
-
 /**
  * This class encapsulates a reading of raw content from an input stream.
  *
@@ -10,26 +7,13 @@ import java.io.InputStream;
  */
 public final class ByteContentReader {
 
-	private static final int CHUNK_SIZE = 4096;
-
+	/**
+	 * Just disables an instance creation.
+	 *
+	 * @throws UnsupportedOperationException always.
+	 */
 	private ByteContentReader() {
 		throw new UnsupportedOperationException();
-	}
-
-	public static ByteContent read(final InputStream in) throws IOException { // TODO: THE PLAN IS TO AVOID IOException
-		final ByteContent result = new AggregatedByteContent();
-
-		try {
-			final byte buffer[] = new byte[CHUNK_SIZE];
-			int bytesRead;
-			while ((bytesRead = in.read(buffer)) > 0) {
-				result.addContent(new BasicByteContent(buffer, bytesRead));
-			}
-		} finally {
-			in.close();
-		}
-
-		return result;
 	}
 
 }
