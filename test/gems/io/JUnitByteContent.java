a9c4eb74401355b10fbcc755ad90bbc9a2289f9b
jozef.babjak <jozef.babjak@gmail.com>
2008-12-29 18:08:54 +0000

    Unit tests for ByteContent interface.

---
+++ test/gems/io/JUnitByteContent.java
@@ -0,0 +1,29 @@
+package gems.io;
+
+import static gems.io.ByteContent.EMPTY_CONTENT;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit tests for the {@code ByteContent} interface.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
+public final class JUnitByteContent {
+
+	/**
+	 * Checks empty content's length.
+	 */
+	@Test public void checkEmptyContentLength() {
+		Assert.assertEquals(0, EMPTY_CONTENT.length());
+		Assert.assertEquals(0, EMPTY_CONTENT.getBytes().length);
+	}
+
+	/**
+	 * Checks whether the emtpy content is immutable.
+	 */
+	@Test(expected = UnsupportedOperationException.class) public void checkEmptyContentImmutability() {
+		EMPTY_CONTENT.addContent(EMPTY_CONTENT);
+	}
+
+}
