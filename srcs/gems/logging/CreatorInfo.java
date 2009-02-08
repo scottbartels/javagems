ac0105c16e613c2cfc16a0de90938305b46df100
jozef.babjak <jozef.babjak@gmail.com>
2009-02-08 16:03:04 +0000

    Finding caller fixed.

--- srcs/gems/logging/CreatorInfo.java
+++ srcs/gems/logging/CreatorInfo.java
@@ -2,7 +2,7 @@ package gems.logging;
 
 /**
  * Encapsulates information about a logging record creator.
- * 
+ *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class CreatorInfo {
@@ -14,25 +14,25 @@ public final class CreatorInfo {
 	 */
 	private static final String CURRENT_PACKAGE = CreatorInfo.class.getPackage().getName();
 
-    /**
-     * A creator's class name.
-     */
-    private final String className;
+	/**
+	 * A creator's class name.
+	 */
+	private final String className;
 
-    /**
-     * A creator's method name.
-     */
-    private final String methodName;
+	/**
+	 * A creator's method name.
+	 */
+	private final String methodName;
 
-    /**
-     * A creator's line number.
-     */
-    private final int lineNumber;
+	/**
+	 * A creator's line number.
+	 */
+	private final int lineNumber;
 
-    /**
-     * Creates a new creator info object.
-     */
-    CreatorInfo() {
+	/**
+	 * Creates a new creator info object.
+	 */
+	CreatorInfo() {
 		final StackTraceElement creator = findCaller();
 		if (creator != null) {
 			className = creator.getClassName() != null ? creator.getClassName() : EMPTY_STRING;
@@ -45,30 +45,30 @@ public final class CreatorInfo {
 		}
 	}
 
-    /**
-     * Returns a creator class name or an empty string if unknown. This method never returns {@code null}.
-     *
-     * @return a creator class name or an empty string if unknown.
-     */
+	/**
+	 * Returns a creator class name or an empty string if unknown. This method never returns {@code null}.
+	 *
+	 * @return a creator class name or an empty string if unknown.
+	 */
 	public String getClassName() {
 		return className;
 	}
 
-    /**
-     * Returns a creator method name or an empty string if unknown. This method never returns {@code null}.
-     *
-     * @return a creator method name or an empty string if unknown.
-     */
-    public String getMethodName() {
+	/**
+	 * Returns a creator method name or an empty string if unknown. This method never returns {@code null}.
+	 *
+	 * @return a creator method name or an empty string if unknown.
+	 */
+	public String getMethodName() {
 		return methodName;
 	}
 
-    /**
-     * Returns a creator line number or zero if inknown.
-     *
-     * @return a creator line number or zero if inknown.
-     */
-    public int getLineNumber() {
+	/**
+	 * Returns a creator line number or zero if inknown.
+	 *
+	 * @return a creator line number or zero if inknown.
+	 */
+	public int getLineNumber() {
 		return lineNumber;
 	}
 
@@ -77,10 +77,11 @@ public final class CreatorInfo {
 	 *
 	 * @return a caller producing the logging record.
 	 */
-    private static StackTraceElement findCaller() {
-		for (final StackTraceElement element : Thread.currentThread().getStackTrace()) {
-			if (!element.getClassName().startsWith(CURRENT_PACKAGE)) {
-				return element;
+	private static StackTraceElement findCaller() {
+		final StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+		for (int i = trace.length - 2; i > 0; i--) {
+			if (trace[i].getClassName().startsWith(CURRENT_PACKAGE)) {
+				return trace[i + 1];
 			}
 		}
 		return null;
