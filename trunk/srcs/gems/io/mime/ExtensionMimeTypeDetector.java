41b23604c5a4f69b41530e2feac5ef6441f639ec
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-02-18 11:59:13 +0000

    A next nasty usecase for extensions handling. Several TODOs added.

--- trunk/srcs/gems/io/mime/ExtensionMimeTypeDetector.java
+++ trunk/srcs/gems/io/mime/ExtensionMimeTypeDetector.java
@@ -20,6 +20,8 @@ public final class ExtensionMimeTypeDetector implements ContextMimeTypeDetector 
 
 	private final String separator;
 
+	// TODO: INJECT OPTIONAL FUNCTINALITY FOR LOCAL FILESYSTEM DETECTION OF DIRECTORIES.
+
 	public ExtensionMimeTypeDetector() {
 		this(File.separator);
 	}
@@ -41,19 +43,22 @@ public final class ExtensionMimeTypeDetector implements ContextMimeTypeDetector 
 		if (context.endsWith(separator)) {
 			return new Option<MimeType>(MimeType.getMimeType("application/directory"));
 		}
+
 		// TODO: Here are use cases:
-		//                          INPUT    FILENAME            EXTENSION     WHAT TO DO      MIME
-		//                             "" ->            (handle as a special case)          -> _unknown_
-		//                            "/" -> _empty_          -> _unknown_  -> directory    -> application/directory
-		//                          "var" -> "var"            -> "var"      -> _detect_     -> _unknown_
-		//                         "/var" -> "var"            -> "var"      -> _detect_     -> _unknown_
-		//                         "var/" -> _empty_          -> _unknown_  -> directory    -> application/directory
-		//                         ".svn" -> "svn"            -> "svn"      -> _detect_     -> _unknown_
-		//                     "Makefile" -> "Makefile"       -> "Makefile" -> _detect_     -> text/x-makefile
-		//               "Linux.Makefile" -> "Linux.Makefile" -> "Makefile" -> _detect_     -> text/x-makefile
-		//        "/usr/home/user/my.doc" -> "my.doc"         -> "doc"      -> _detect_     -> application/msword
-		//       "/usr/home/user/my.doc/" -> _empty_          -> unknown    -> directory"   -> application/directory
-		// "/usr/home/user/my.doc/me.png" -> "me.png"         -> "png"      -> _detect_     -> image/png
+		//                          INPUT    FILENAME            EXTENSION      WHAT TO DO      MIME
+		//                             "" ->            (handle as a special case)           -> _unknown_
+		//                            "/" -> _empty_          -> _unknown_   -> directory    -> application/directory
+		//                          "var" -> "var"            -> "var"       -> _detect_     -> _unknown_
+		//                         "/var" -> "var"            -> "var"       -> _detect_     -> _unknown_
+		//                         "var/" -> _empty_          -> _unknown_   -> directory    -> application/directory
+		//                         ".svn" -> "svn"            -> "svn"       -> _detect_     -> _unknown_
+		//                     "Makefile" -> "Makefile"       -> "Makefile"  -> _detect_     -> text/x-makefile
+		//               "Linux.Makefile" -> "Linux.Makefile" -> "Makefile"  -> _detect_     -> text/x-makefile
+		//        "/usr/home/user/my.doc" -> "my.doc"         -> "doc"       -> _detect_     -> application/msword
+		//       "/usr/home/user/my.doc." -> "my.doc."        -> _empty_     -> _unknown_    -> _unknown_
+		//       "/usr/home/user/my.doc/" -> _empty_          -> unknown     -> directory    -> application/directory
+		// "/usr/home/user/my.doc/me.png" -> "me.png"         -> "png"       -> _detect_     -> image/png
+
 		return new Option<MimeType>(null);
 	}
 
--- trunk/srcs/gems/io/mime/MimeUtils.java
+++ trunk/srcs/gems/io/mime/MimeUtils.java
@@ -2,15 +2,17 @@ package gems.io.mime;
 
 import gems.io.ByteContent;
 
-final class MimeUtils {
+@Deprecated final class MimeUtils {
 
+	// TODO: THIS MIGHT NOT BE HERE. BUT: WHERE? PROBABLY IN MimeType. 
 	static final MimeType GLOBAL_DEFAULT_MIME_TYPE = MimeType.getMimeType("application/octet-stream");
 
 	static ByteContent context2content(final String context) {
 		if (context == null) {
 			throw new IllegalArgumentException();
 		}
-		return ByteContent.EMPTY_CONTENT;
+		// TODO: THIS OPERATION IS QUITE SENSITIVE. THERE MIGHT BE MORE REASONABLE STRATEGIES HOW TO DO THIS, INCLUDING NONE.
+		return ByteContent.EMPTY_CONTENT; // TODO: RETURN Option<ByteContent>
 	}
 
 }
