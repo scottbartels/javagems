abbc41595a5a1733dabee5c3d9a0ed625e6d9d7d
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-02-18 09:02:51 +0000

    MimeType is identifiable now.

--- trunk/srcs/gems/io/mime/MimeType.java
+++ trunk/srcs/gems/io/mime/MimeType.java
@@ -1,52 +1,31 @@
 package gems.io.mime;
 
-public final class MimeType {
+import gems.AbstractIdentifiable;
 
-	private static final String DELIMITER = "/";
+public final class MimeType extends AbstractIdentifiable<String> {
 
-	private final String type;
-
-	private final String subtype;
-
-	private MimeType(final String type, final String subtype) {
-		assert type != null;
-		assert subtype != null;
-		this.type = type;
-		this.subtype = subtype;
+	private MimeType(final String spec) {
+		super(spec);
 	}
 
 	static MimeType getMimeType(final String spec) {
 		if (spec == null) {
 			throw new IllegalArgumentException();
 		}
-		final String[] elements = spec.split(DELIMITER); // todo: use precompiled Pattern for better performance
-		if (elements.length != 2) {
-			throw new IllegalArgumentException(); // todo: custom exception?
-		}
-		return getMimeType(elements[0], elements[1]);
-	}
-
-	static MimeType getMimeType(final String type, final String subtype) {
-		if (type == null) {
-			throw new IllegalArgumentException();
-		}
-		if (subtype == null) {
-			throw new IllegalArgumentException();
-		}
-		// TODO: ADD CACHING HERE
-		return new MimeType(type, subtype);
+		// todo: check specification
+		// todo: add chaching
+		return new MimeType(spec);
 	}
 
 	public String getType() {
-		return type;
+		return getId().split("/")[0];
 	}
 
 	public String getSubtype() {
-		return subtype;
+		return getId().split("/")[1];
 	}
 
 	@Override public String toString() {
-		return type + DELIMITER + subtype;
+		return getId();
 	}
-
 }
