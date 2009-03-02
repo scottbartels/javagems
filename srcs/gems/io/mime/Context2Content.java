4a97e681f8c8516f28048d4e58ddb405db55672c
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-03-02 07:20:47 +0000

    Context2Content is now public. It is fully documented, including its implementations.

--- srcs/gems/io/mime/Context2Content.java
+++ srcs/gems/io/mime/Context2Content.java
@@ -3,14 +3,45 @@ package gems.io.mime;
 import gems.io.ByteContent;
 import gems.Option;
 
-interface Context2Content<T> {
+/**
+ * Converts a context of type {@code T} to a byte content. Because the
+ * conversion is quite sensitive operation which may not be possible at
+ * all, resulting byt content is encapsulated in {@code Option} wrapper
+ * forcing client code to check a content presence.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @param <T> a type of context
+ */
+public interface Context2Content<T> {
 
+	/**
+	 * A null-implementation of the interface. The conversion operation is
+	 * always unsuccessful, i.e. an empty {@code Option} is always returned.
+	 */
 	Context2Content<Object> NULL_IMPLEMENTATION = new Context2Content<Object>() {
+
+		/**
+		 * Always returns an empty {@code Option} for any non-null context.
+		 *
+		 * @param context converted context.
+		 * @return an empty {@code Option}, always.
+		 *
+		 * @throws IllegalArgumentException if {@code context} is {@code null}. 
+		 */
 		@Override public Option<ByteContent> context2content(final Object context) {
+			if (context == null) {
+				throw new IllegalArgumentException();
+			}
 			return new Option<ByteContent>(null);
 		}
 	};
 
+	/**
+	 * Converts given context to byte content, if possible.
+	 *
+	 * @param context converted context.
+	 * @return byte content encapsulated in {@code Option} wrapper. 
+	 */
 	Option<ByteContent> context2content(T context);
 
 }
--- srcs/gems/io/mime/LocalFilesystemContext2Content.java
+++ srcs/gems/io/mime/LocalFilesystemContext2Content.java
@@ -9,6 +9,12 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.File;
 
+/**
+ * Gets byte content by reading a local file specified by a context.
+ * Context is interpreted as a path to a file.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
 public class LocalFilesystemContext2Content implements Context2Content<String> {
 
 	/**
@@ -16,8 +22,10 @@ public class LocalFilesystemContext2Content implements Context2Content<String> {
 	 * If a file resource given by {@code context} cannot be found, cannot be read or is
 	 * not a regural file, an empty option is returned.
 	 *
-	 * @param context
-	 * @return
+	 * @param context a path to a local file.
+	 *
+	 * @return content of a given file.
+	 * @throws IllegalArgumentException if {@code context} is {@code null}.
 	 */
 	@Override public Option<ByteContent> context2content(final String context) {
 		if (context == null) {
