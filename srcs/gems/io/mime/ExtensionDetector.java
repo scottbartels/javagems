8288cd8a4d5681b73404484f175c546b6676b441
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-03-04 11:36:27 +0000

    Interface documented.

--- srcs/gems/io/mime/ExtensionDetector.java
+++ srcs/gems/io/mime/ExtensionDetector.java
@@ -1,7 +1,20 @@
 package gems.io.mime;
 
+/**
+ * An extension detector gets a filename extension from givne context information.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @param <T> type of context.
+ */
 interface ExtensionDetector<T> {
 
-	String detect(T path);
+	/**
+	 * Analyses given context and tries to determine a filename extension.
+	 *
+	 * @param context analyzed context.
+	 *
+	 * @return filename extension detected from a given context.
+	 */
+	String detect(T context);
 
 }
