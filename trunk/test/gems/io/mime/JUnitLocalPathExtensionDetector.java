fd443e4c16ae9d447449825194f69e7545822aff
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-03-04 12:15:59 +0000

    Fixture inlined. Javadoc added.

--- trunk/test/gems/io/mime/JUnitLocalPathExtensionDetector.java
+++ trunk/test/gems/io/mime/JUnitLocalPathExtensionDetector.java
@@ -1,33 +1,55 @@
 package gems.io.mime;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
 
+/**
+ * Unit tests for {@code LocalPathExtensionDetector} class.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
 public final class JUnitLocalPathExtensionDetector {
 
-	private final ExtensionDetector<String> fixture = new LocalPathExtensionDetector("/", "."); 
-
+	/**
+	 * Checks whether {@code null} path separator is forbidden.
+	 */
 	@Test(expected = IllegalArgumentException.class) public void nullPathSeparatorIsForbidden() {
 		new LocalPathExtensionDetector(null, ".");
 	}
 
+	/**
+	 * Checks whether empty path separator is forbidden.
+	 */
 	@Test(expected = IllegalArgumentException.class) public void emptyPathSeparatorIsForbidden() {
 		new LocalPathExtensionDetector("", ".");
 	}
 
+	/**
+	 * Checks whether {@code null} extension separator is forbidden.
+	 */
 	@Test(expected = IllegalArgumentException.class) public void nullExtensionSeparatorIsForbidden() {
 		new LocalPathExtensionDetector("/", null);
 	}
 
+	/**
+	 * Checks whether empty extension separator is forbidden.
+	 */
 	@Test(expected = IllegalArgumentException.class) public void emptyExtensionSeparatorIsForbidden() {
 		new LocalPathExtensionDetector("/", "");
 	}
 
-	@Test(expected = IllegalArgumentException.class) public void nullPathIsForbidden() {
-		fixture.detect(null);
+	/**
+	 * Checks whether {@code null} context is forbidden.
+	 */
+	@Test(expected = IllegalArgumentException.class) public void nullContextIsForbidden() {
+		new LocalPathExtensionDetector("/", ".").detect(null);
 	}
 
+	/**
+	 * Checks extension detection for various paths using obvious separators.
+	 */
 	@Test public void testVariousPahts() {
+		final ExtensionDetector<String> fixture = new LocalPathExtensionDetector("/", ".");
 		Assert.assertEquals("", fixture.detect(""));
 		Assert.assertEquals("", fixture.detect("/"));
 		Assert.assertEquals("txt", fixture.detect("file.txt"));
@@ -40,6 +62,9 @@ public final class JUnitLocalPathExtensionDetector {
 		Assert.assertEquals("", fixture.detect("/working/copy/.svn/"));
 	}
 
+	/**
+	 * Checks behavior for various path separators.
+	 */
 	@Test public void testVarioiusPathSeparators() {
 		final String path = "a/b\\c::d";
 		Assert.assertEquals("b\\c::d", new LocalPathExtensionDetector("/", ".").detect(path));
@@ -50,6 +75,9 @@ public final class JUnitLocalPathExtensionDetector {
 		Assert.assertEquals("", new LocalPathExtensionDetector(path, ".").detect(path));
 	}
 
+	/**
+	 * Checks behavior for various extension separators.
+	 */
 	@Test public void testVariousExtensionSeparators() {
 		final String path = "a/b\\c::d";
 		Assert.assertEquals("a/b\\c::d", new LocalPathExtensionDetector("@", ".").detect(path));
@@ -59,5 +87,5 @@ public final class JUnitLocalPathExtensionDetector {
 		Assert.assertEquals(":d", new LocalPathExtensionDetector("@", "c:").detect(path));
 		Assert.assertEquals("", new LocalPathExtensionDetector("@", path).detect(path));
 	}
-	
+
 }
