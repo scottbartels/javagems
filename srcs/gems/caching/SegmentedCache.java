26d3d00fb1149fec423670d3858c866be801e740
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-03-05 11:04:01 +0000

    Segmented cache is done.

--- srcs/gems/caching/SegmentedCache.java
+++ srcs/gems/caching/SegmentedCache.java
@@ -6,12 +6,34 @@ import gems.Option;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Segmented cache implementation. It holds a flat cache for
+ * each segment and delegates operations to these segments.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @param <V> type of cached values.
+ * @param <K> type of keys.
+ */
 final class SegmentedCache<V extends Identifiable<K>, K> implements Cache<V, K> {
 
+	/**
+	 * A segmenter.
+	 */
 	private final CacheSegmenter<K> segmenter;
 
+	/**
+	 * Segments.
+	 */
 	private final List<Cache<V, K>> segments;
 
+	/**
+	 * Creates a new segmented cache. 
+	 *
+	 * @param evicter passed to flat cache segments as is.
+	 * @param sizer passed to flat cache segments as is.
+	 * @param limits passed to flat cache segments splitting limits across them.
+	 * @param segmenter a segmenter defining a segmentation for the cache.
+	 */
 	SegmentedCache(
 			final CacheEvicter<?> evicter,
 			final SizeEstimator<V> sizer,
@@ -26,6 +48,12 @@ final class SegmentedCache<V extends Identifiable<K>, K> implements Cache<V, K> 
 		}
 	}
 
+	/**
+	 * Finds appropriate segment for a given key.
+	 *
+	 * @param id a key.
+	 * @return appropriate segment for a given key. 
+	 */
 	private Cache<V, K> getSegment(final K id) {
 		return segments.get(segmenter.getSegment(id));
 	}
@@ -43,4 +71,5 @@ final class SegmentedCache<V extends Identifiable<K>, K> implements Cache<V, K> 
 		}
 		return getSegment(id).get(id);
 	}
+	
 }
