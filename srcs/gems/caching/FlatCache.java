ecd1c894e7379d56517dd5ccff45546f19d65692
jozef.babjak <jozef.babjak@gmail.com>
2009-03-12 20:57:53 +0000

    Heavyweight locking for a flat cache: multiple simultaneous lookups are possible now.

--- srcs/gems/caching/FlatCache.java
+++ srcs/gems/caching/FlatCache.java
@@ -5,9 +5,16 @@ import gems.Identifiable;
 import gems.Option;
 import gems.SizeEstimator;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 final class FlatCache<V extends Identifiable<K>, K> implements Cache<V, K> {
 
 	private static final int DELAY = 10; // TODO: INJECT
+
+	private final ReadWriteLock lock = new ReentrantReadWriteLock(true);
 
 	private final CacheEvicter<K> evicter;
 
@@ -21,44 +28,87 @@ final class FlatCache<V extends Identifiable<K>, K> implements Cache<V, K> {
 		new EvictSchedulerDaemon(DELAY).start();
 	}
 
-	@Override public synchronized void offer(final V object) {
+	@Override public void offer(final V object) {
 		if (object == null) {
 			throw new IllegalArgumentException();
 		}
-		storage.put(object);
+		lock.writeLock().lock();
+		try {
+			storage.put(object);
+		} finally {
+			lock.writeLock().unlock();
+		}
 	}
 
-	@Override public synchronized Option<V> get(final K id) {
+	@Override public Option<V> get(final K id) {
 		if (id == null) {
 			throw new IllegalArgumentException();
 		}
-		return storage.get(id);
+		lock.readLock().lock();
+		try {
+			return storage.get(id);
+		} finally {
+			lock.readLock().unlock();
+		}
 	}
-	
-	// TODO: USE SYNCHRONIZATION ALLOWING SEVERAL CONCURRENT LOOKUPS
 
-	private synchronized void evict() {
-		storage.evict(evicter.evict(storage.itemsForEviction()));
-	}
+	private final class EvictSchedulerDaemon extends Thread {
 
-	private final class EvictSchedulerDaemon extends Thread { // TODO: SOMETHING SMARTER MIGHT BE USEFUL FOR HIGH LOADS
+		// TODO: SOMETHING SMARTER FOR HIGH LOADS?
+		//
+		// 1) delay might be adaptive. If no items are evicted in one run,
+		//    delay might be increased. If some items are evicted [in N subsequent
+		//    runs, delay might be decreased.
+		//
+		// 2) frequency of adding new items might be observed and this information
+		//    can be used somehow.
+		//
+		// 3) wait-notify mechamizm might be used instead of sleeping. 
 
 		private final long delay;
 
 		private EvictSchedulerDaemon(final int delay) {
-			this.delay = delay * 1000;
+			this.delay = delay * 1000L;
 			this.setDaemon(true);
 		}
 
-		@SuppressWarnings({"InfiniteLoopStatement"})
-		@Override public void run() {
-			while (true) {
-				try {
-					Thread.sleep(delay);
-				} catch (final InterruptedException e) {
-					ExceptionHandler.NULL_HANDLER.handle(e); // TODO: IS THERE ANY SMARTER OPTION?
-				}
+		/**
+		 * Periodically invokes eviction. 
+		 */
+		@Override @SuppressWarnings({"InfiniteLoopStatement"}) public void run() {
+			while (true) { // TODO: COULD BE STOPPED BY FINALIZER OF ENCAPSULATING CLASS?
+				sleep();
 				evict();
+			}
+		}
+
+		/**
+		 * Sleeps.  
+		 */
+		private void sleep() {
+			try {
+				Thread.sleep(delay);
+			} catch (final InterruptedException e) {
+				ExceptionHandler.NULL_HANDLER.handle(e); // TODO: IS THERE ANY SMARTER OPTION?
+			}
+		}
+
+		/**
+		 * Performs eviction on storage.
+		 */
+		private void evict() {
+			// Everything must be done holding the write lock. It ensures that there is
+			// not any reader active during eviction. It is because a reading operation
+			// is modifying cached items statistics data, but an eviction algorithm
+			// usually highly depends on them.
+			lock.writeLock().lock();
+			try {
+				final Collection<K> keysToEvict = evicter.evict(storage.itemsForEviction());
+				if (!keysToEvict.isEmpty()) {
+					storage.evict(Collections.unmodifiableCollection(keysToEvict));
+				}
+			} finally {
+				lock.writeLock().unlock();
 			}
 		}
 
