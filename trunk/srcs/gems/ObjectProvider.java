9e53011ec518aa918230a7b3d44bbe40c619f661
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-03-14 11:22:40 +0000

    Adding JavaDoc.

--- trunk/srcs/gems/ObjectProvider.java
+++ trunk/srcs/gems/ObjectProvider.java
@@ -1,9 +1,29 @@
 package gems;
 
+/**
+ * A factory-like interface defining a type-safe object provider, which creates
+ * objects of certain type {@code T} according to some kind of key of type {@code K}.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @param <T> type of provided objects.
+ * @param <K> type of provided objects identifiers.
+ */
 @Experimental public interface ObjectProvider<T, K> {
 
+	/**
+	 * A null-implementation of the interface. It does not provide anything.
+	 */
 	public ObjectProvider<Object, Object> NULL_PROVIDER = new ObjectProvider<Object, Object>() {
 
+		/**
+		 * Provides nothing. An empty {@code Option} is always returned.
+		 *
+		 * @param key ignored.
+		 *
+		 * @return always an empty {@code Option} object.
+		 *
+		 * @throws IllegalArgumentException if {@code key} is {@code null}.
+		 */
 		public Option<Object> provide(final Object key) {
 			if (key == null) {
 				throw new IllegalArgumentException();
@@ -14,16 +34,16 @@ package gems;
 	};
 
 	/**
-	 * Returns an object idnetified by the given key. The returned
+	 * Returns an object according to a given key. The returned
 	 * object is encapsulated to {@code Option} object, so the client
 	 * has to check whether a required object was provided or not.
 	 * Because of this, the method should never return {@code null}.
 	 *
-	 * @param id an ID of required object.
+	 * @param key a key identifying requested object.
 	 *
 	 * @return an optional value holding a provided object or empty
 	 *         option if required object cannot be provided.
 	 */
-	Option<T> provide(K id);
+	Option<T> provide(K key);
 
 }
