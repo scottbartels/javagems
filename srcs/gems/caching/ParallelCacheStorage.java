f26f42775cbb54f0415894e0dfb06bc8c37487c7
jozef.babjak <jozef.babjak@gmail.com>
2009-03-15 20:19:17 +0000

    Parallel collecting of evictable items. Using a thread pool instead of explicitly started threads.

--- srcs/gems/caching/ParallelCacheStorage.java
+++ srcs/gems/caching/ParallelCacheStorage.java
@@ -1,17 +1,21 @@
 package gems.caching;
 
+import gems.ExceptionHandler;
 import gems.Identifiable;
 import gems.Option;
 import gems.SizeEstimator;
-import gems.ExceptionHandler;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.*;
 
 final class ParallelCacheStorage<K, V extends Identifiable<K>> implements CacheStorage<K, V> {
 
 	private final Collection<CacheStorage<K, V>> storages;
+
+	private final ExecutorService pool;
 
 	ParallelCacheStorage(final SizeEstimator<V> sizer) {
 		assert sizer != null;
@@ -20,69 +24,100 @@ final class ParallelCacheStorage<K, V extends Identifiable<K>> implements CacheS
 		for (int i = 0; i < cpus; i++) {
 			storages.add(new FlatCacheStorage<K, V>(sizer));
 		}
+		pool = Executors.newFixedThreadPool(cpus);
 	}
 
-	public Option<V> get(final K key) {
+	@Override public Option<V> get(final K key) {
 		// todo: 1) get from all storages in parallel
 		// todo: 2) ensure that at most one was found (assert?)
 		// todo: 3) if one was found, re-pack into new Option and return
 		return new Option<V>(null);
 	}
 
-	public void put(final V value) {
+	@Override public void put(final V value) {
 		// todo: 1) try to get
 		// todo: 2) if found, update in the right storage and finish
 		// todo: 3) otherwise select storage
 		// todo: 4) put value into
 	}
 
-	public Collection<CacheItem<K>> itemsForEviction() {
-		// todo: 1) get times for eviction from all storages in parallel
-		// todo: 2) merge them together and return.
-		return new LinkedList<CacheItem<K>>();
-	}
-
-	public void evict(final Collection<K> keys) {
-		final Collection<Thread> excutors = new LinkedList<Thread>();
-		for (CacheStorage<K, V> storage : storages) {
-			final Thread thread = new Thread(new EvictionExecutor<K>(storage, keys));
-			thread.start();
-			excutors.add(thread);
+	@Override public Collection<CacheItem<K>> itemsForEviction() {
+		final Collection<Future<Collection<CacheItem<K>>>> tasks = new LinkedList<Future<Collection<CacheItem<K>>>>();
+		for (final CacheStorage<K, V> storage : storages) {
+			tasks.add(pool.submit(new GatheringEvictablesTask<K>(storage)));
 		}
-		randezVous(excutors);
-	}
-
-	private static void randezVous(final Iterable<? extends Thread> excutors) {
+		final List<CacheItem<K>> result = new LinkedList<CacheItem<K>>();
 		try {
-			for(final Thread thread : excutors) {
-				thread.join();
+			for (final Future<Collection<CacheItem<K>>> task : tasks) {
+				result.addAll(task.get());
 			}
 		} catch (final InterruptedException e) {
+			ExceptionHandler.NULL_HANDLER.handle(e);
+		} catch (final ExecutionException e) {
+			ExceptionHandler.NULL_HANDLER.handle(e);
+		}
+		return result;
+	}
+
+	@Override public void evict(final Collection<K> keys) {
+		if (keys == null) {
+			throw new IllegalArgumentException();
+		}
+		final Collection<Future<?>> tasks = new LinkedList<Future<?>>();
+		for (final CacheStorage<K, V> storage : storages) {
+			tasks.add(pool.submit(new EvictionTask<K>(storage, keys)));
+		}
+		try {
+			for (final Future<?> task : tasks) {
+				task.get();
+			}
+		} catch (final InterruptedException e) {
+			ExceptionHandler.NULL_HANDLER.handle(e);
+		} catch (final ExecutionException e) {
 			ExceptionHandler.NULL_HANDLER.handle(e);
 		}
 	}
 
 
-	private static final class EvictionExecutor<T> implements Runnable {
+	private static abstract class AbstractStorageTask<T> {
 
 		private final CacheStorage<T, ? extends Identifiable<T>> storage;
 
+		protected AbstractStorageTask(final CacheStorage<T, ? extends Identifiable<T>> storage) {
+			this.storage = storage;
+		}
+
+		protected CacheStorage<T, ? extends Identifiable<T>> getStorage() {
+			return storage;
+		}
+
+	}
+
+	private static final class EvictionTask<T> extends AbstractStorageTask<T> implements Runnable {
+
 		private final Collection<T> keys;
 
-		private EvictionExecutor(
-				final CacheStorage<T, ? extends Identifiable<T>> storage,
-				final Collection<T> keys
-		) {
-			this.storage = storage;
+		private EvictionTask(final CacheStorage<T, ? extends Identifiable<T>> storage, final Collection<T> keys) {
+			super(storage);
 			this.keys = keys;
 		}
 
 		@Override public void run() {
-			storage.evict(keys);
+			getStorage().evict(keys);
 		}
-		
+
 	}
 
-	
+	private static final class GatheringEvictablesTask<T> extends AbstractStorageTask<T> implements Callable<Collection<CacheItem<T>>> {
+
+		private GatheringEvictablesTask(final CacheStorage<T, ? extends Identifiable<T>> storage) {
+			super(storage);
+		}
+
+		@Override public Collection<CacheItem<T>> call() throws Exception {
+			return getStorage().itemsForEviction();
+		}
+
+	}
 
 }
