69095cafa0b9d4cbfbf194ab9109ffb8c21d4f13
jozef.babjak <jozef.babjak@gmail.com>
2009-03-20 14:17:06 +0000

    Adding javadoc. Adding searching capabilities.

--- srcs/gems/storage/MemoryStorage.java
+++ srcs/gems/storage/MemoryStorage.java
@@ -1,9 +1,14 @@
 package gems.storage;
 
 import gems.Identifiable;
+import gems.filtering.Filter;
 
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Collections;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public final class MemoryStorage<K, V extends Identifiable<K>> implements Storage<K, V> {
 
@@ -30,4 +35,20 @@ public final class MemoryStorage<K, V extends Identifiable<K>> implements Storag
 		map.remove(key);
 	}
 
+	@Override public Collection<V> search(final Filter<V> filter) {
+		if (filter == null) {
+			throw new IllegalArgumentException();
+		}
+		final Collection<V> result = new LinkedList<V>();
+		for (final V value : map.values()) {
+			if (filter.allows(value)) {
+				result.add(value);
+			}
+		}
+		return result;
+	}
+
+	@Override public Iterator<V> iterator() {
+		return Collections.unmodifiableCollection(map.values()).iterator();
+	}
 }
--- srcs/gems/storage/Storage.java
+++ srcs/gems/storage/Storage.java
@@ -1,13 +1,33 @@
 package gems.storage;
 
 import gems.Identifiable;
+import gems.filtering.Filter;
 
-public interface Storage<K, V extends Identifiable<K>> {
+import java.util.Collection;
+
+public interface Storage<K, V extends Identifiable<K>> extends Iterable<V> {
 
 	void put(V value);
 
-	V get(K key);
+	V get(K key);  // todo: maybe storage IS object provider. 
 
-	void remove(K key);
+	/**
+	 * Guarantees that object identified by a givne key is not in the storage
+	 * after successful return. No contract for pre-conditions is specified by
+	 * the interface, it is up to implementation what to do when no object with
+	 * that key is found in the storage.
+	 *
+	 * @param key a key of removed object.
+	 */
+	void remove(K key); 
+
+	/**
+	 * Returns a collection of stored values which satisfy a condistion specified by a given filter.
+	 * This method should never return {@code null}, at least empty collection should be returned.
+	 *
+	 * @param filter a filter of returned values.
+	 * @return  a collection of stored values which satisfy a condistion specified by a given filter.
+	 */
+	Collection<V> search(Filter<V> filter);
 	
 }
