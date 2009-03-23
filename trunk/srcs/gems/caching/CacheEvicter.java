1c1002ee7289fbdbb7447c5c5a20dbfd208f3dbf
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-03-23 19:22:23 +0000

    Interface documented.

--- trunk/srcs/gems/caching/CacheEvicter.java
+++ trunk/srcs/gems/caching/CacheEvicter.java
@@ -4,8 +4,24 @@ import gems.Limits;
 
 import java.util.Collection;
 
-public interface CacheEvicter<K> {
+/**
+ * Cache eviction strategy. Implementation have to be thread-safe, ideally re-entrant. 
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
+public interface CacheEvicter<K> { // todo: rename to CacheEvictor
 
-	Collection<K> evict(Collection<CacheItemStatistics<K>> items, Limits<CacheLimit> limits);
+	/**
+	 * Evaluates given collection of cache item statistics and selects
+	 * some of them - if any - for eviction as necessary for satisfying
+	 * given cache limits. This method should never return {@code null};
+	 * at least an empty collection has to returned even if no items are
+	 * selected for eviction.
+	 *
+	 * @param statistics a collection of cache item statistics.
+	 * @param limits cache limits to satisfy.
+	 * @return a collection - possibly an empty one - of cache item keys selected for eviction.
+	 */
+	Collection<K> evict(Collection<CacheItemStatistics<K>> statistics, Limits<CacheLimit> limits);
 
 }
