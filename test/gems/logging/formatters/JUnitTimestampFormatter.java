b7ce99a893a3ad17beb3431aa8d27f43f0a96b62
jozef.babjak <jozef.babjak@gmail.com>
2009-05-05 18:05:20 +0000

    Using thread local for TimestampFormatter, including thread-safeness test.

--- test/gems/logging/formatters/JUnitTimestampFormatter.java
+++ test/gems/logging/formatters/JUnitTimestampFormatter.java
@@ -3,12 +3,18 @@ package gems.logging.formatters;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 /**
  * Unit tests for the {@code TimestampFormatter} class.
  *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
  */
 public final class JUnitTimestampFormatter {
+
+	private static final int CHECKERS = 4;
+	private static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
 
 	/**
 	 * Checks whether a {@code null} pattern is forbidden.
@@ -21,7 +27,68 @@ public final class JUnitTimestampFormatter {
 	 * Checks whether a formatter does not return {@code null} value.
 	 */
 	@Test public void returnsNotNull() {
-		Assert.assertNotNull(new TimestampFormatter("yyyy-MM-dd HH:mm:ss").format(System.currentTimeMillis()));
+		Assert.assertNotNull(new TimestampFormatter(PATTERN).format(System.currentTimeMillis()));
+	}
+
+	@Test public void checkThreadSafeness() {
+		final Checker[] checkers = new Checker[CHECKERS];
+
+		final TimestampFormatter fixture = new TimestampFormatter(PATTERN);
+
+		for (int i = 0; i < CHECKERS; i++) {
+			checkers[i] = new Checker(fixture, i + 1);
+			checkers[i].start();
+		}
+
+		final long start = System.currentTimeMillis();
+
+		for (final Checker checker : checkers) {
+			try {
+				checker.join();
+			} catch (final InterruptedException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		final long end = System.currentTimeMillis();
+
+		System.err.println("Execution done in " + (end - start) + "ms.");
+
+		for (final Checker checker : checkers) {
+			Assert.assertTrue(checker.getResult());
+		}
+
+	}
+
+	private static final class Checker extends Thread {
+
+		private final TimestampFormatter fixture;
+
+		private final long time;
+
+		private final String expectedResult;
+
+		private volatile boolean result = true;
+
+		private Checker(final TimestampFormatter fixture, final int div) {
+			this.fixture = fixture;
+			this.time = System.currentTimeMillis() / div;
+			this.expectedResult = new SimpleDateFormat(PATTERN).format(new Date(time));
+		}
+
+
+		@Override public void run() {
+			for (int i = 0; i < 1000000; i++) {
+				if (!expectedResult.equals(fixture.format(time))) {
+					result = false;
+					return;
+				}
+			}
+		}
+
+		private boolean getResult() {
+			return result;
+		}
 	}
 
 }
