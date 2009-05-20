6d26e4ca44dbe1edaf5e1f119f08051572cf78b6
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-05-20 14:38:52 +0000

    Improving Cache documentation.

--- trunk/srcs/gems/caching/Cache.java
+++ trunk/srcs/gems/caching/Cache.java
@@ -4,26 +4,20 @@ import gems.Identifiable;
 import gems.ObjectProvider;
 
 /**
- * A cache is a likely transient storage of indentifiable objects
- * which can be retrieved back according their identifiers.
- * Stored objects may be evicted meantime by cache internal
- * processes, so the client have to be ready for the situation
- * when previously stored object is not in cache anymore. This
- * interface provides an abstraction for a cache: it allows to
- * store indentifiable objects and to try to retrieve them back
- * in type-safe manner. If you are using a cache, probably
- * the {@code gesm.caching.CachingObjectProvider} wrapper can
- * do your life even easyier.
+ * A cache is a likely transient storage of indentifiable objects which can be retrieved  back
+ * according their identifiers. Stored objects may be evicted meantime by cache internal processes,
+ * so the client have to be ready for the situation when previously stored object is not in the cache
+ * anymore. If you are using a cache, probably the {@code gesm.caching.CachingObjectProvider} wrapper
+ * can do your life even easier.
  *
  * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
- * @param <K> a type of keys.
- * @param <V> a type of objects.
+ * @param <V> a type of cached objects.
+ * @param <K> a type of keys indetifying cached objects.
  */
 public interface Cache<V extends Identifiable<K>, K> extends ObjectProvider<V, K> {
 
 	/**
-	 * Offers a given object for a caching. The object may or
-	 * may not be cached, depending on a cache implementation.
+	 * Offers a given object for a caching. The object may or may not be cached, depending on a cache implementation.
 	 *
 	 * @param object an object offered for a caching.
 	 */
