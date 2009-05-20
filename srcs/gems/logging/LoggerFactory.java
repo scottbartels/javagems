4b032f6656bbd0dc15b272aea1f3bc0415cbe2fd
jozef.babjak <jozef.babjak@gmail.com>
2009-05-20 13:29:44 +0000

    Adding logger factory.

---
+++ srcs/gems/logging/LoggerFactory.java
@@ -0,0 +1,35 @@
+package gems.logging;
+
+import gems.logging.loggers.ParallelLogger;
+import gems.logging.loggers.AsynchronousLogger;
+import gems.logging.loggers.SynchronousLogger;
+
+
+public interface LoggerFactory { 
+
+	LoggerFactory NULL_LOGGER_FACTORY = new LoggerFactory() {
+
+		public Logger get() {
+			return Logger.NULL_LOGGER;
+		}
+
+	};
+
+	LoggerFactory DEFAULT_LOGGER_FACTORY = new LoggerFactory() {
+
+		public Logger get() {
+			final int cpus = Runtime.getRuntime().availableProcessors();
+			if (cpus > 4) {
+				return new ParallelLogger();
+			}
+			if (cpus > 1) {
+				return new AsynchronousLogger();
+			}
+			return new SynchronousLogger();
+		}
+
+	};
+
+	Logger get();
+
+}
