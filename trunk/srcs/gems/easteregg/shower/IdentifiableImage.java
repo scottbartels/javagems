629cd3e762e68554143a44054c2cfd85f80a52dc
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-06-10 15:15:44 +0000

    Splitting Shower into pieces.

---
+++ trunk/srcs/gems/easteregg/shower/IdentifiableImage.java
@@ -0,0 +1,23 @@
+package gems.easteregg.shower;
+
+import gems.AbstractIdentifiable;
+
+import java.awt.Image;
+
+final class IdentifiableImage extends AbstractIdentifiable<String> {
+
+	private final Image image;
+
+	IdentifiableImage(final String path, final Image image) {
+		super(path);
+		if (image == null) {
+			throw new IllegalArgumentException();
+		}
+		this.image = image;
+	}
+
+	Image getImage() {
+		return image;
+	}
+
+}
