977c81bd5feed5581deed125bfe654e458ac54bf
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-06-10 15:22:57 +0000

    Image loading moved to proper location.

--- trunk/srcs/gems/easteregg/shower/ImageProvider.java
+++ trunk/srcs/gems/easteregg/shower/ImageProvider.java
@@ -3,6 +3,8 @@ package gems.easteregg.shower;
 import gems.ObjectProvider;
 import gems.Option;
 
+import java.awt.*;
+
 final class ImageProvider implements ObjectProvider<IdentifiableImage, String> {
 
 	public Option<IdentifiableImage> provide(final Option<String> key) {
@@ -11,9 +13,18 @@ final class ImageProvider implements ObjectProvider<IdentifiableImage, String> {
 		}
 		if (key.hasValue()) {
 			final String path = key.getValue();
-			return new Option<IdentifiableImage>(new IdentifiableImage(path, Shower.loadImage(path)));
+			return new Option<IdentifiableImage>(new IdentifiableImage(path, loadImage(path)));
 		}
 		return new Option<IdentifiableImage>(null);
 	}
 
+	private static Image loadImage(final String path) {
+		final Image image = Toolkit.getDefaultToolkit().getImage(path);
+		// TODO: RESIZE IMAGE TO FIT IT INTO A SCREEN
+//		final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+//		if (image.getHeight(null) > screen.getHeight() || image.getWidth(null) > screen.getWidth()) {
+//			return image.getScaledInstance((int)screen.getWidth(), (int)screen.getHeight(), Image.SCALE_DEFAULT);
+//		}
+		return image;
+	}
 }
