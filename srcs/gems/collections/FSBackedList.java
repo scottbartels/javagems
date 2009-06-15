fa2241d2300923c40755b95764ec3880b263caec
jozef.babjak <jozef.babjak@gmail.com>
2009-06-15 18:13:31 +0000

    Collection interface is mostly implemented now.

--- srcs/gems/collections/FSBackedList.java
+++ srcs/gems/collections/FSBackedList.java
@@ -3,35 +3,34 @@ package gems.collections;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * Storing {@code null} objects is not permitted. Stored objects should not rely on defalut {@code equals()} method.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ */
 public final class FSBackedList<E extends Serializable> implements Collection<E>, RandomAccess {
 
 	private final List<Head<E>> heads = new ArrayList<Head<E>>();
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public int size() {
 		return heads.size();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	public boolean isEmpty() {
 		return heads.isEmpty();
 	}
 
-	public boolean contains(final Object o) {
-		for (final Head<E> head : heads) {
-			if (head.getData().equals(o)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public Object[] toArray() {
-		final Object[] result = new Object[heads.size()];
-		for (int i = 0; i < heads.size(); i++) {
-			result[i] = heads.get(i).getData();
-		}
-		return result;
-	}
-
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException if {@code e} is {@code null}.
+	 */
 	public boolean add(final E e) {
 		if (e == null) {
 			throw new IllegalArgumentException();
@@ -39,17 +38,44 @@ public final class FSBackedList<E extends Serializable> implements Collection<E>
 		return heads.add(new Head<E>(e));
 	}
 
-	public boolean remove(final Object o) {
-		for (final Head<E> head : heads) {
-			if (head.getData().equals(o)) {
-				heads.remove(head);
-				return true;
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean contains(final Object o) {
+		if (o != null) {
+			for (final Head<E> head : heads) {
+				if (head.getData().equals(o)) {
+					return true;
+				}
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean remove(final Object o) {
+		if (o != null) {
+			for (int i = 0; i < heads.size(); i++) {
+				if (heads.get(i).getData().equals(o)) {
+					heads.remove(i);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException if {@code c} is {@code null}.
+	 */
 	public boolean containsAll(final Collection<?> c) {
+		if (c == null) {
+			throw new IllegalArgumentException();
+		}
 		for (final Object o : c) {
 			if (!contains(o)) {
 				return false;
@@ -58,32 +84,39 @@ public final class FSBackedList<E extends Serializable> implements Collection<E>
 		return true;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException if {@code c} is {@code null}.
+	 */
 	public boolean addAll(final Collection<? extends E> c) {
+		if (c == null) {
+			throw new IllegalArgumentException();
+		}
 		if (c.isEmpty()) {
 			return false;
 		}
 		for (final E e : c) {
-			heads.add(new Head<E>(e));
+			add(e);
 		}
 		return true;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException if {@code c} is {@code null}.
+	 */
 	public boolean removeAll(final Collection<?> c) {
-		boolean changed = false;
-		for (final Object o : c) {
-			final boolean removed = remove(o);
-			if (removed) {
-				changed = true;
-			}
+		if (c == null) {
+			throw new IllegalArgumentException();
 		}
-		return changed;
-	}
-
-	public boolean retainAll(final Collection<?> c) {
+		if (c.isEmpty()) {
+			return false;
+		}
 		boolean changed = false;
 		for (int i = heads.size() - 1; i >= 0; i--) {
-			final Head<E> head = heads.get(i);
-			if (c.contains(head.getData())) {
+			if (c.contains(heads.get(i).getData())) {
 				heads.remove(i);
 				changed = true;
 			}
@@ -91,22 +124,59 @@ public final class FSBackedList<E extends Serializable> implements Collection<E>
 		return changed;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @throws IllegalArgumentException if {@code c} is {@code null}.
+	 */
+	public boolean retainAll(final Collection<?> c) {
+		if (c == null) {
+			throw new IllegalArgumentException();
+		}
+		if (isEmpty()) {
+			return false;
+		}
+		if (c.isEmpty()) {
+			clear();
+			return true;
+		}
+		boolean changed = false;
+		for (int i = heads.size() - 1; i >= 0; i--) {
+			if (!c.contains(heads.get(i).getData())) {
+				heads.remove(i);
+				changed = true;
+			}
+		}
+		return changed;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
 	public void clear() {
 		heads.clear();
 	}
 
-	public E get(final int index) {
-		return heads.get(index).getData();
-	}
+//	public E get(final int index) {
+//		return heads.get(index).getData();
+//	}
 
 	// TODO: NOT YET IMPLEMENTED PART OF THE INTERFACE
 
 	public Iterator<E> iterator() {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+		return null;
 	}
 
-	public <T> T[] toArray(T[] a) {
-		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	public Object[] toArray() { // todo: violates contract if not synchronized
+		final Object[] result = new Object[heads.size()];
+		for (int i = 0; i < heads.size(); i++) {
+			result[i] = heads.get(i).getData();
+		}
+		return result;
+	}
+
+	public <T> T[] toArray(final T[] a) {
+		return null;
 	}
 
 	// HEAD
