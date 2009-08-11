5b92c1ac9d4d27d4a69f347c6b5ec5f3ec90c263
jozef.babjak <jozef.babjak@gmail.com>
2009-08-11 08:30:12 +0000

    The EvictionHandler interface is now top-level member.

--- srcs/gems/caching/CacheEvictor.java
+++ srcs/gems/caching/CacheEvictor.java
@@ -2,7 +2,6 @@ package gems.caching;
 
 import gems.Limits;
 
-import java.awt.*;
 import java.util.Collection;
 
 /**
@@ -13,57 +12,18 @@ import java.util.Collection;
  */
 public interface CacheEvictor<K> {
 
-	public interface EvictionHandler<T> {
-
-		public static final EvictionHandler<?> NULL_EVICTION_HANDLER = new EvictionHandler<Object>() {
-
-			/**
-			 * Does nothing.
-			 * `
-			 *
-			 * @param value {@inheritDoc}
-			 *
-			 * @throws IllegalArgumentException if {@code value} is {@code null}.
-			 */
-			public void handle(final Object value) {
-				if (value == null) {
-					throw new IllegalArgumentException();
-				}
-			}
-		};
-
-		public static final EvictionHandler<Image> AWT_IMAGE_EVICTION_HANDLER = new EvictionHandler<Image>() {
-
-			public void handle(final Image value) {
-				if (value == null) {
-					throw new IllegalArgumentException();
-				}
-				value.flush();
-			}
-
-		};
-
-		/**
-		 * Handles evicted value.
-		 *
-		 * @param value an evicted value.
-		 */
-		void handle(T value);
-
-	}
-
-	/**
-	 * Evaluates given collection of cache item statistics and selects
-	 * some of them - if any - for eviction as necessary for satisfying
-	 * given cache limits. This method should never return {@code null};
-	 * at least an empty collection has to returned even if no items are
-	 * selected for eviction.
-	 *
-	 * @param statistics a collection of cache item statistics.
-	 * @param limits cache limits to satisfy.
-	 *
-	 * @return a collection - possibly an empty one - of cache item keys selected for eviction.
-	 */
-	Collection<K> evict(Collection<CacheItemStatistics<K>> statistics, Limits<CacheLimit> limits);
+    /**
+     * Evaluates given collection of cache item statistics and selects
+     * some of them - if any - for eviction as necessary for satisfying
+     * given cache limits. This method should never return {@code null};
+     * at least an empty collection has to returned even if no items are
+     * selected for eviction.
+     *
+     * @param statistics a collection of cache item statistics.
+     * @param limits cache limits to satisfy.
+     *
+     * @return a collection - possibly an empty one - of cache item keys selected for eviction.
+     */
+    Collection<K> evict(Collection<CacheItemStatistics<K>> statistics, Limits<CacheLimit> limits);
 
 }
