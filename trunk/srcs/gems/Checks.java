e169e67b56a0de2fbaa298850d60dc8c3bcecf64
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-08-20 17:12:09 +0000

    Documenting recent check methods.

--- trunk/srcs/gems/Checks.java
+++ trunk/srcs/gems/Checks.java
@@ -1,21 +1,50 @@
 package gems;
 
-public final class Checks {
+/**
+ * Useful checks.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @since CURRENT
+ */
+@Experimental public final class Checks {
 
-    private Checks() {
-        throw new UnsupportedOperationException();
-    }
+	/**
+	 * Just disables an instance creation. This is the utility class.
+	 *
+	 * @throws UnsupportedOperationException always.
+	 */
+	private Checks() {
+		throw new UnsupportedOperationException();
+	}
 
-    public static <T> T assertNotNull(final T object) {
-        assert object != null;
-        return object;
-    }
+	/**
+	 * Checks that given object is not {@code null} using assertion and returns the same object.
+	 * If assertions are not used, this is identity function. If assertions are used and {@code null}
+	 * argument is given, this method does not returns normally.
+	 *
+	 * @param object a checked object.
+	 *
+	 * @return returns the same object.
+	 */
+	public static <T> T assertNotNull(final T object) {
+		assert object != null;
+		return object;
+	}
 
-    public static <T> T ensureNotNull(final T object) {
-        if (object == null) {
-            throw new NullPointerException();
-        }
-        return object;
-    }
+	/**
+	 * Returns the same object if it is not {@code null} or throws {@code NullPointerException} otherwise.
+	 *
+	 * @param object the same object.
+	 *
+	 * @return the same object.
+	 *
+	 * @throws NullPointerException if {@code object} is {@code null}.
+	 */
+	public static <T> T ensureNotNull(final T object) {
+		if (object == null) {
+			throw new NullPointerException();
+		}
+		return object;
+	}
 
 }
