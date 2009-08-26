c62118d2411ecce6d67d55adb80d506194fad921
jozef.babjak@gmail.com <jozef.babjak@gmail.com@4040058f-f653-0410-8a6a-63227756becb>
2009-08-26 16:59:11 +0000

    Adding javadoc explaining that empty package.

--- trunk/srcs/gems/storage/package-info.java
+++ trunk/srcs/gems/storage/package-info.java
@@ -1,3 +1,9 @@
+/**
+ * Content of this package is temporarly made to be internal to gems.caching package.
+ * At the moment, it is the only place where it is used. It has to be refactored and
+ * maybe even re-designed before publishing. This empty package is kept here just to
+ * remember.
+ */
 @Experimental package gems.storage;
 
 import gems.Experimental;
