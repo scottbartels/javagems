9404e76ce160a7503c4c617809e5d2a0dd83deb8
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-10-20 11:02:35 +0000

    Adding javadoc

--- srcs/gems/caching/LeastRecentlyUsedEvictorFactory.java
+++ srcs/gems/caching/LeastRecentlyUsedEvictorFactory.java
@@ -6,9 +6,23 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+/**
+ * Provides <em>last-recenlty used</em> evictors.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a>
+ * @param <K> type of cache objects identifiers.
+ */
 public final class LeastRecentlyUsedEvictorFactory<K> implements CacheEvictorFactory<K> {
 
+	/**
+	 * Returns a <em>last-recently used</em> evictor. This method never returns {@code null}.
+	 *
+	 * @return a <em>last-recently used</em> evictor.
+	 */
 	public CacheEvictor<K> get() {
+
+		// todo: with the current implementation, result can be cached.
+		
 		final List<Comparator<CacheItemStatistics<K>>> comparators = new ArrayList<Comparator<CacheItemStatistics<K>>>(2);
 		comparators.add(new LeastRecentlyUsedEvictionComparator<K>());
 		comparators.add(new LeastFrequentlyUsedEvictionComparator<K>());
