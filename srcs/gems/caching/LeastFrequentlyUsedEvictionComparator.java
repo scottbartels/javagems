dfc72dd0b25f4ef2eeb91a1a34715a268a2558a1
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-10-21 04:30:50 +0000

    Unnecessary constructors removed. Serializaion UID added.

--- srcs/gems/caching/LeastFrequentlyUsedEvictionComparator.java
+++ srcs/gems/caching/LeastFrequentlyUsedEvictionComparator.java
@@ -1,12 +1,11 @@
 package gems.caching;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-final class LeastFrequentlyUsedEvictionComparator<K> implements Comparator<CacheItemStatistics<K>> {
-
-	LeastFrequentlyUsedEvictionComparator() {
-		// really nothing here
-	}
+final class LeastFrequentlyUsedEvictionComparator<K> implements Comparator<CacheItemStatistics<K>>, Serializable {
+	
+	private static final long serialVersionUID = 7099838009626557398L;
 
 	@Override public int compare(final CacheItemStatistics<K> x, final CacheItemStatistics<K> y) {
 		return (int) (y.getAccesses() - x.getAccesses());
