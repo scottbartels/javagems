5af182061632ecfafc8b7c78b859741aeb56b2a7
jozef.babjak <jozef.babjak@4040058f-f653-0410-8a6a-63227756becb>
2009-11-03 12:59:57 +0000

    Adding javadoc.

--- trunk/srcs/gems/NumericValueOutOfRangeException.java
+++ trunk/srcs/gems/NumericValueOutOfRangeException.java
@@ -1,18 +1,34 @@
 package gems;
 
 /**
+ * This exception is thrown when a numeric value checked by a business logic is not within expected boundaries.
+ *
+ * @author <a href="mailto:jozef.babjak@gmail.com">Jozef BABJAK</a> 
  * @since 2009.11
  */
 @Experimental public class NumericValueOutOfRangeException extends RuntimeException {
 
+    /**
+     * Creates a new instance of the exception.
+     */
 	public NumericValueOutOfRangeException() {
 		super();
 	}
 
+    /**
+     * Creates a new instance of the exception.
+     *
+     * @param value a numeric value causing troubles.
+     */
 	public NumericValueOutOfRangeException(final Number value) {
 		this(value == null ? null : value.toString());
 	}
 
+    /**
+     * Creates a new instance of the exception.
+     *
+     * @param message an exception message.
+     */
 	public NumericValueOutOfRangeException(final String message) {
 		super(message);
 	}
